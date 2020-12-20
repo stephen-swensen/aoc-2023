9b44a0a362cf5e7f1681982f9e332976d0b107d8
Stephen Swensen <stephen.swensen@gmail.com>
2020-12-20 17:53:36 -0500

    solve d01p1

---
+++ d01p1.fs
@@ -0,0 +1,21 @@
+﻿module D01P1
+
+let readInput fileName =
+    let lines = System.IO.File.ReadAllLines fileName
+    lines 
+    |> Seq.map int
+    |> Seq.toArray
+
+
+let run fileName =
+    let input = readInput fileName
+    let indices = Seq.init input.Length id
+    let possibilities =
+        (indices, indices)
+        ||> Seq.allPairs
+        |> Seq.filter (fun (x,y) -> x <> y)
+    let (x,y) =
+        possibilities
+        |> Seq.find (fun (x,y) -> 
+            input.[x] + input.[y] = 2020)
+    input.[x] * input.[y]
