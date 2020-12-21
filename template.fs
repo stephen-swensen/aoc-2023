18e54bce38359b53cf2f939e5fb5cfbe1194f149
Stephen Swensen <stephen.swensen@gmail.com>
2020-12-21 16:35:23 -0500

    solve day 3 part 1

---
+++ template.fs
@@ -0,0 +1,20 @@
+﻿module TEMPLATE
+
+open System
+
+let parseInput inputReader =
+    let lines = inputReader.ReadAllLines ()
+    lines
+
+
+let run inputReader =
+    let input = parseInput inputReader
+    input
+
+module Tests =
+    open NUnit.Framework
+    open Swensen.Unquote
+
+    [<Test; Ignore("Template Test")>]
+    let ``test`` () =
+        true =! true
