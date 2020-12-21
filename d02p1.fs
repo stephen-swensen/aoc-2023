2ae70de5d75ca1f0d3d3c8a8ba7e686cd8b929c3
Stephen Swensen <stephen.swensen@gmail.com>
2020-12-20 20:00:34 -0500

    start d02p1

---
+++ d02p1.fs
@@ -0,0 +1,36 @@
+﻿module D02P1
+
+open System
+
+type Policy = { Min: int; Max: int; Letter: char }
+
+let parseLine (line:string) = 
+    let parts = line.Split([|'-'; ' '; ':'|], StringSplitOptions.RemoveEmptyEntries)
+    { Min=parts.[0] |> int
+      Max=parts.[1] |> int
+      Letter=parts.[2] |> char }, parts.[3]
+
+
+let readInput fileName =
+    let lines = System.IO.File.ReadAllLines fileName
+    lines 
+    |> Seq.map parseLine
+    |> Seq.toList
+
+let run fileName =
+    let input = readInput fileName
+    0
+
+module Tests =
+    open NUnit.Framework
+    open Swensen.Unquote
+    [<Test>]
+    let ``parseLine`` () =
+        let line = "2-7 s: qwdngzbtsntgzmxz"
+        let actual = parseLine line
+        let expected = 
+            { Min=2
+              Max=7
+              Letter='s' }, "qwdngzbtsntgzmxz"
+
+        actual =! expected
