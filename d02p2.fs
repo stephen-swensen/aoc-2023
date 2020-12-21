6cfec31a3319d1cb8441c7f9049d05f2f85b8c91
Stephen Swensen <stephen.swensen@gmail.com>
2020-12-21 00:44:46 -0500

    solve day2 part2

---
+++ d02p2.fs
@@ -0,0 +1,30 @@
+﻿module D02P2
+
+open System
+
+type Policy = { Pos1: int; Pos2: int; Letter: char }
+
+let parseLine (line:string) =
+    let parts = line.Split([|'-'; ' '; ':'|], StringSplitOptions.RemoveEmptyEntries)
+    { Pos1=(parts.[0] |> int) - 1
+      Pos2=(parts.[1] |> int) - 1
+      Letter=parts.[2] |> char }, parts.[3]
+
+let readInput fileName =
+    let lines = System.IO.File.ReadAllLines fileName
+    lines
+    |> Seq.map parseLine
+    |> Seq.toList
+
+let countValidPasswords input =
+    let validPasswords =
+        input
+        |> Seq.filter (fun (policy, password:string) ->
+            xor (password.[policy.Pos1] = policy.Letter)
+                (password.[policy.Pos2] = policy.Letter))
+
+    validPasswords |> Seq.length
+
+let run fileName =
+    let input = readInput fileName
+    countValidPasswords input
--- prelude.fs
+++ prelude.fs
@@ -1,6 +1,9 @@
 [<AutoOpen>]
 module Prelude
+#nowarn "42"
 
 ///A correct mod implementation (whereas (%) is the remainder operator and may produce negative results)
 let inline (%%) n m =
     ((n % m) + m) % m
+
+let inline xor (x:bool) (y:bool) = (# "xor" x y : bool #)
