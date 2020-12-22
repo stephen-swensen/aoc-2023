18f009553d8f52157c788c903a8f7536aa6c13c3
Stephen Swensen <stephen.swensen@gmail.com>
2020-12-21 23:45:00 -0500

    add ReadAllText to InputReader

--- prelude.fs
+++ prelude.fs
@@ -21,8 +21,13 @@ let inline (%%) n m =
 let inline xor (x:bool) (y:bool) = (# "xor" x y : bool #)
 
 ///An interface for puzzel file input
-type InputReader = { ReadAllLines: unit -> string [] } with
+type InputReader = {
+    ReadAllLines: unit -> string []
+    ReadAllText: unit -> string
+} with
     static member FromFile path =
-        { ReadAllLines = (fun () -> IO.File.ReadAllLines path) }
+        { ReadAllLines = (fun () -> IO.File.ReadAllLines path)
+          ReadAllText = (fun () -> IO.File.ReadAllText path) }
     static member FromString (text:string) =
-        { ReadAllLines = (fun () -> text.Split([|"\r\n"; "\n"|], StringSplitOptions.None) ) }
+        { ReadAllLines = (fun () -> text.Split([|"\r\n"; "\n"|], StringSplitOptions.None) )
+          ReadAllText = (fun () -> text) }
