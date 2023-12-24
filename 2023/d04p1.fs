c16e89597120ddac86a3b93a7bd18c2e9e17327d
Stephen Swensen <stephen.swensen@gmail.com>
2023-12-23 23:40:03 -0500

    prep d04p1

---
+++ 2023/d04p1.fs
@@ -0,0 +1,22 @@
+﻿module D04P1
+
+open System
+
+let parseInput inputReader =
+  let lines = inputReader.ReadAllLines()
+  lines
+
+let run inputReader =
+  let input = parseInput inputReader
+  0
+
+//----------------------------------------------------------
+//Tests
+open NUnit.Framework
+open Swensen.Unquote
+
+[<Test>]
+let ``sample input test`` () =
+  let text = """"""
+  let reader = InputReader.FromString text
+  run reader =! 0
